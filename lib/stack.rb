9400226d44d0f6fb4f998ab069fd10c765f411f9
Sean Handley <sean.handley@gmail.com>
2016-07-29 07:08:11 +0100

    Stack tests.

--- lib/stack.rb
+++ lib/stack.rb
@@ -19,26 +19,3 @@ class Stack
     @stack.count
   end
 end
-
-# def input
-#   (1..1000).to_a
-# end
-
-# def reversed_input
-#   input.reverse
-# end
-
-# def test
-#   stack = Stack.new
-#   input.each do |element|
-#     stack.push element
-#   end
-#   output = []
-#   while(el = stack.pop)
-#     output << el
-#   end
-#   raise "Fail!" unless reversed_input == output
-#   puts "Success!"
-# end
-
-# test
