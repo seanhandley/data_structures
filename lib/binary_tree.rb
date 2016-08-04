cf48225cd07b741658e27630bc3e60c0364e6729
Sean Handley <sean.handley@gmail.com>
2016-08-04 20:15:27 +0100

    Refactor.

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -27,26 +27,7 @@ module DataStructures
       def draw
         @graph = GraphViz.new(:G, :type => :digraph)
         @visited = []
-        action = -> (node) {
-          root, left, right = nil, nil, nil
-          if node.element
-            root = @graph.add_nodes(node.element.to_s)
-            @visited << node.element.to_s unless @visited.include?(node.element.to_s)
-          end
-          if node&.left&.element
-            left = @graph.add_nodes(node.left.element.to_s)
-            left.set{|n| n.color = "green"}
-            @visited << node.left.element.to_s unless @visited.include?(node.left.element.to_s)
-            @graph.add_edges(root, left)
-          end
-          if node&.right&.element
-            right = @graph.add_nodes(node.right.element.to_s)
-            right.set{|n| n.color = "red"}
-            @visited << node.right.element.to_s unless @visited.include?(node.right.element.to_s)
-            @graph.add_edges(root, right)
-          end
-        }
-        traverse_node(self, action)
+        traverse_node(self, create_nodes_and_edges)
         @graph.output( :png => "binary_tree.png" )
       end
 
@@ -75,6 +56,28 @@ module DataStructures
             node.right = BinaryTree::Node.new(e)
           end
         end
+      end
+
+      def create_nodes_and_edges
+        -> (node) {
+          root, left, right = nil, nil, nil
+          if node.element
+            root = @graph.add_nodes(node.element.to_s)
+            @visited << node.element.to_s unless @visited.include?(node.element.to_s)
+          end
+          if node&.left&.element
+            left = @graph.add_nodes(node.left.element.to_s)
+            left.set{|n| n.color = "green"}
+            @visited << node.left.element.to_s unless @visited.include?(node.left.element.to_s)
+            @graph.add_edges(root, left)
+          end
+          if node&.right&.element
+            right = @graph.add_nodes(node.right.element.to_s)
+            right.set{|n| n.color = "red"}
+            @visited << node.right.element.to_s unless @visited.include?(node.right.element.to_s)
+            @graph.add_edges(root, right)
+          end
+        }
       end
     end
   end
